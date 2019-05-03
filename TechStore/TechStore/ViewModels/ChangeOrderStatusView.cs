9bf6177a99a366d6683f82a5d74d65141d034282
IvanPilyak <pilyak.vanya@gmail.com>
2019-05-03 19:40:57 +0300

    Added OrderController.

---
+++ TechStore/TechStore/ViewModels/ChangeOrderStatusView.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using TechStore.DAL.Classes;
+
+namespace TechStore.ViewModels
+{
+    public class ChangeOrderStatusView
+    {
+        public int Id { get; set; }
+
+        public OrderStatus CurrentStatus { get; set; }
+
+        public List<OrderStatus> OrderStatuses { get; set; }
+
+        public bool SendEmail { get; set; }
+    }
+}
---
+++ TechStore/TechStore/ViewModels/ConfirmOrderView.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using TechStore.DAL.Models;
+
+namespace TechStore.ViewModels
+{
+    public class ConfirmOrderView
+    {
+        public int CustomerId { get; set; }
+
+        public Customer Customer { get; set; }
+
+        public Country Country { get; set; }
+
+        public List<OrderPart> Goods { get; set; }
+
+        public int Count { get; set; }
+
+        public List<Storage> Storages { get; set; }
+
+        public int CommonPrice { get; set; }
+
+        public string EndPointCity { get; set; }
+
+        public string EndPointStreet { get; set; }
+    }
+}
