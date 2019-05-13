70968df03477f0a3273de4a9e97d4af450e90d2d
IvanPilyak <pilyak.vanya@gmail.com>
2019-05-13 17:19:40 +0300

    Tasted Repositories.

---
+++ TechStore/TechStore.NUnit.Tests/Repositories/StorageRepositoryTests.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using TechStore.DAL.Classes.UnitOfWork.Classes;
+using TechStore.DAL.Models;
+
+namespace TechStore.NUnit.Tests.Repositories
+{
+    [TestFixture]
+    class StorageRepositoryTests
+    {
+        DbContextOptions<AppDbContext> options;
+        [SetUp]
+        public void Setup()
+        {
+            options = new DbContextOptionsBuilder<AppDbContext>()
+               .UseInMemoryDatabase(databaseName: "InMemoryDatabase").Options;
+        }
+
+        [Test]
+        public async Task CreateTest()
+        {
+            //Arrange
+            using (var context = new AppDbContext(options))
+            {
+                var storage = new Storage { Id = 1 };
+                var repo = new StorageRepository(context);
+
+                //Act
+                await repo.Create(storage);
+                var expectedResult = 1;
+                var actualResult = context.Storages.Local.Count;
+
+                //Assert
+                Assert.AreEqual(expectedResult, actualResult);
+            }
+        }
+
+        [Test]
+        public async Task DeleteTest()
+        {
+            //Arrange
+            using (var context = new AppDbContext(options))
+            {
+                var storage = new Storage { Id = 1 };
+                var repo = new StorageRepository(context);
+
+                //Act
+                context.Storages.Add(storage);
+                await repo.Delete(storage.Id);
+                var expectedResult = 0;
+                var actualResult = context.Storages.Local.Count;
+
+                //Assert
+                Assert.AreEqual(expectedResult, actualResult);
+            }
+        }
+
+        [Test]
+        public async Task GetTest()
+        {
+            //Arrange
+            using (var context = new AppDbContext(options))
+            {
+                var storage = new Storage { Id = 1 };
+                var repo = new StorageRepository(context);
+
+                //Act
+                context.Storages.Add(storage);
+                var expectedResult = await repo.Get(storage.Id);
+                var actualResult = storage;
+
+                //Assert
+                Assert.AreEqual(expectedResult, actualResult);
+            }
+        }
+
+        [Test]
+        public void GetAllTest()
+        {
+            //Arrange
+            using (var context = new AppDbContext(options))
+            {
+                var storages = new List<Storage>{ new Storage{ Id = 1 } };
+                var repo = new StorageRepository(context);
+
+                //Act
+                foreach (var storage in storages)
+                {
+                    context.Add(storage);
+                }
+
+                context.SaveChanges();
+                var expectedResult = repo.GetAll();
+                var actualResult = storages;
+
+                //Assert
+                Assert.AreEqual(expectedResult, actualResult);
+            }
+        }
+
+        [Test]
+        public void UpdateTest()
+        {
+            //Arrange
+            using (var context = new AppDbContext(options))
+            {
+                var storage = new Storage { Id = 1 };
+                var repo = new StorageRepository(context);
+
+                //Act
+                repo.Update(storage);
+                var actualResult = context.Entry(storage).State;
+                var expectedResult = EntityState.Modified;
+
+                //Assert
+                Assert.AreEqual(expectedResult, actualResult);
+            }
+        }
+    }
+}
